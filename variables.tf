521a9ede477e370852c2e09d8b19b5b8fcb63f7f
Satoshi Fukuda <satoshi.fukuda@hashicorp.com>
2026-08-02 09:46:28 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,23 +8,3 @@ variable "domain_name" {
   description = "Route53 で管理するドメイン名"
   type        = string
 }
-
-variable "aws_vpc_id" {
-  description = "aws vpc id"
-  type        = string
-}
-
-variable "client_public_ip" {
-  description = "Client EC2 のパブリックIP"
-  type        = string
-}
-
-variable "server_private_ip" {
-  description = "Server EC2 のプライベートIP"
-  type        = string
-}
-
-variable "hosted_zone_id" {
-  description = "Route53 Hostzone ID"
-  type        = string
-}
