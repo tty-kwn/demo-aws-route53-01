b391334fee01ffc98c6d360d3872d8b5cfeee130
Satoshi Fukuda <satoshi.fukuda@hashicorp.com>
2026-08-02 09:50:32 +0900

    Define variables for EC2 and Vault access
    Added variables for EC2 configuration and access control.

--- variables.tf
+++ variables.tf
@@ -3,6 +3,35 @@ variable "aws_region" {
   type        = string
   default     = "ap-northeast-1"
 }
+variable "allowed_ssh_cidr" {
+  description = "CIDR allowed to access EC2 via SSH"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "allowed_vault_cidr" {
+  description = "CIDR allowed to access Vault API"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0244fcbfd244dd3fa"
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ec2_key_name" {
+  description = "EC2 key pair name"
+  type        = string
+  default     = "vault-poc-keypair"
+}
 
 variable "domain_name" {
   description = "Route53 で管理するドメイン名"
