7be082187a2391fd47e41a90868bd8552737ad0d
Tetsuya Kawano <85481899+tty-kwn@users.noreply.github.com>
2026-08-05 18:35:58 +0900

    Remove allowed SSH and Vault CIDR variables
    Removed variables for allowed SSH and Vault CIDR.

--- variables.tf
+++ variables.tf
@@ -3,17 +3,6 @@ variable "aws_region" {
   type        = string
   default     = "ap-northeast-1"
 }
-variable "allowed_ssh_cidr" {
-  description = "CIDR allowed to access EC2 via SSH"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "allowed_vault_cidr" {
-  description = "CIDR allowed to access Vault API"
-  type        = string
-  default     = "10.0.0.0/16"
-}
 
 variable "domain_name" {
   description = "Route53 で管理するドメイン名"
