3e8e5f8781b0ac11621f76be84c2a1cc49dc14d3
Tetsuya Kawano <85481899+tty-kwn@users.noreply.github.com>
2026-08-05 18:27:00 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,22 +15,6 @@ variable "allowed_vault_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "ec2_ami_id" {
-  description = "AMI ID for EC2 instances"
-  type        = string
-}
-
-variable "ec2_instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "ec2_key_name" {
-  description = "EC2 key pair name"
-  type        = string
-}
-
 variable "domain_name" {
   description = "Route53 で管理するドメイン名"
   type        = string
